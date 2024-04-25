1fff2270a6f8b4013c989fada249bcae2c13c56c
Shania Cabrera <136392662+shania-cabrera@users.noreply.github.com>
2024-04-25 15:13:12 +0000

    fixed build, https://shania-cabrera.github.io/class-25-pwa/

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ const key = ref("chicago");
 </script>
 
 <template>
+  <p>meow</p>
   <main>
     <img :src="`${baseDir}images/${fixtures[key].img}`">
     <h1>Filename: {{ fixtures[key].img }}</h1>
