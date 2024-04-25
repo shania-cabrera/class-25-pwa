488138b884d62afe65a0ad9870e257cb71e56cd2
Shania Cabrera <136392662+shania-cabrera@users.noreply.github.com>
2024-04-25 14:59:05 +0000

    build + published

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.DEPLOY === "github" ? '/class-25-pwa/' : './',
   plugins: [
     vue(),
     VitePWA({
